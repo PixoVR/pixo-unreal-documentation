5e3679b879a7da5bcf4e16ad484706cb6974bad7
Chris Healer <chris@eyelash.ai>
2023-07-05 08:49:10 -0600

    replaced empty comment with htmlentity

--- Source/PixoDocumentation/Private/reporter.cpp
+++ Source/PixoDocumentation/Private/reporter.cpp
@@ -1222,6 +1222,7 @@ void reporter::writeNodeBody(FString prefix, UEdGraphNode* n)
 	comment = htmlentities(comment);
 	comment = comment.Replace(TEXT("\r"), TEXT(""));
 	comment = comment.Replace(TEXT("\n"), TEXT("&nbsp;<br/>"));			// after counting lines
+	comment = comment.IsEmpty() ? "&nbsp;" : comment;
 	hasBubble &= !comment.IsEmpty();										// if the comment is empty, it won't show.
 
 	//wcout << "FTITLE: " << *title << " ## " << hasBubble << " * " << (mn?1:0) << " ## " << *comment << endl;
