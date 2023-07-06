ee6a1a9931142fc6244319a31bb4ef95e76d0c05
Chris Healer <chris@eyelash.ai>
2023-07-05 18:21:11 -0600

    bugs

--- Source/PixoDocumentation/Private/reporter.cpp
+++ Source/PixoDocumentation/Private/reporter.cpp
@@ -1257,7 +1257,7 @@ void reporter::writeNodeBody(FString prefix, UEdGraphNode* n)
 	//if a comment is visible, add it as a node and connect the arrow
 	if (hasBubble)
 	{
-		float lineHeight = 12.0f;
+		float lineHeight = 10.0f;
 		float margin = 30.0f;
 
 		//float cposx = posx + (type==NodeType::route ? -.228f : 0.0f);	//&reg;
